56b041dfb5e8dd7792958380c59af8bc2169752c
Grant Britchford <145594323+grant-britchford@users.noreply.github.com>
2024-06-26 13:11:30 +0000

    deployment coding readied

--- GreatHall/settings.py
+++ GreatHall/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-gefu^4r*&9+@epgoqelifjt1cm-z$h!j*j9a#ut_*2@c2#2*nz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1',]
+ALLOWED_HOSTS = ['127.0.0.1', 'storesalen-760222f28e5a.herokuapp.com',]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "StoreSalen",
 ]
 
 MIDDLEWARE = [
--- GreatHall/urls.py
+++ GreatHall/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from StoreSalen.views import index_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("index/", index_views.index, name='home'),
 ]
