f62aa40ee0f89f8c4f015aaf9267ca0a0edece2e
Grant Britchford <145594323+grant-britchford@users.noreply.github.com>
2024-06-27 10:34:57 +0000

    new template directory created for registration and log-in.html and log-out.html created and added

--- GreatHall/urls.py
+++ GreatHall/urls.py
@@ -21,5 +21,5 @@ from django.urls import path, include # type: ignore
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("account/", include('account.urls')),
+    #path("account/", include('account.urls')),
 ]
