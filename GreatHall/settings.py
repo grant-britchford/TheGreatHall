b6a8089eaa90373d5a3647c56f6db72770a95e33
Grant Britchford <145594323+grant-britchford@users.noreply.github.com>
2024-06-28 14:02:44 +0000

    sqlitestudio database connected

--- GreatHall/settings.py
+++ GreatHall/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-wkye9+m^-x_4g20%g=7^!6k44&zd!64c!ntqyzs4_1#f&kitm0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com',]
+ALLOWED_HOSTS = ['127.0.0.1', 'storesalen-760222f28e5a.herokuapp.com',]
 
 
 # Application definition
